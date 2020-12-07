587807751e1521cd676302e9e6388fcc6ccd6d32
Aline-Aguiar <aline.aglilly@gmail.com>
2020-12-06 22:59:05 -0300

    alteração em Principal

--- Desafio01/src/br/com/aline/desafio01/Principal.kt
+++ Desafio01/src/br/com/aline/desafio01/Principal.kt
@@ -18,13 +18,10 @@ fun main() {
     sistema.registrarAluno("Paul", "Phillip", 2)
     sistema.registrarAluno("Leandro", "Alves", 3)
     sistema.registrarAluno("Patrícia", "Gimenez", 4)
-    //sistema.registrarAluno("Priscila", "Silva", 5)
 
     sistema.matricularAluno(1, 20001)
     sistema.matricularAluno(3, 20001)
     sistema.matricularAluno(2, 20002)
     sistema.matricularAluno(4, 20002)
-    //istema.matricularAluno(4,20002)
-    //sistema.matricularAluno(5,20002)
 
 }
